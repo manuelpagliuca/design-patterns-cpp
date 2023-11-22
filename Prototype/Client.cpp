08233839c85a3e7f2488dd4f192b84272bd55c33
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-11-22 12:54:32 +0100

    Uncomment Prototype

--- Prototype/Client.cpp
+++ Prototype/Client.cpp
@@ -8,14 +8,14 @@ int main()
 	ConcretePrototypeA* prototypeA = new ConcretePrototypeA();
 	ConcretePrototypeB* prototypeB = new ConcretePrototypeB();
 
-	//Prototype* cloneA = prototypeA->clone();
-	//Prototype* cloneB = prototypeB->clone();
+	Prototype* cloneA = prototypeA->clone();
+	Prototype* cloneB = prototypeB->clone();
 
-	//std::cout << cloneA->type() << std::endl;
-	//std::cout << cloneB->type() << std::endl;
+	std::cout << cloneA->type() << std::endl;
+	std::cout << cloneB->type() << std::endl;
 
-	//delete cloneA;
-	//delete cloneB;
+	delete cloneA;
+	delete cloneB;
 
 	delete prototypeA;
 	delete prototypeB;
